b5a4a2d7010e7ecaf780517cbf7675e06a234579
icechanisara <89003966+icechanisara@users.noreply.github.com>
2021-11-26 11:17:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -160,6 +160,12 @@ def event_handle(event,json_line):
         elif (msg == "เมื่อไหร่จะเลิกจีบคะ") :
             replyObj = TextSendMessage(text="สักวันค่ะแต่ไม่ใช่วันนี้")
             line_bot_api.reply_message(rtoken, replyObj)
+        elif msg == "covid" :
+            url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
+            response = requests.get(url)
+            response = response.json()
+            replyObj = TextSendMessage(text=str(response))
+            line_bot_api.reply_message(rtoken, replyObj)
         else : 
             headers = request.headers
             json_headers = ({k:v for k, v in headers.items()})
